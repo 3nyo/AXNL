9af3114682254e94076c4c815aa947d500a79af1
3nyo <enyo-eris@gmail.com>
2020-11-17 22:58:04 -0800

    Added demo database, entry addition, querying, and linking.

--- src/axnl/core.clj
+++ src/axnl/core.clj
@@ -1,45 +1,92 @@
 (ns axnl.core
   (:gen-class)
-  (:require [crux.api :as crux])
+  (:require [crux.api :as crux]
+            [database.core :as db]
+            [clojure.string :as str])
   (:import [java.util UUID]))
 
 
 
 
-(crux/submit-tx db
-                (mapv (fn [node] [:crux.tx/put node]) nodes))
-
-(sort (crux/q (crux/db db)
-              `{:find [object]
-                :where [[object :name "Test"]]
-                :full-results? true}))
+(def database (db/init-new-db))
 
 (defn add-node
   "Add a node to the dataset."
   [name description]
-  (crux/submit-tx db
+  (crux/submit-tx database
                   [[:crux.tx/put
                     {:crux.db/id (UUID/randomUUID)
                      :name name
                      :description description
-                     :type :node}]]))
+                     :type :object}]]))
 
 (defn search-nodes
   "Search nodes by their names first and text content second."
-  [words]
-  (crux/q (crux/db db)
-          `(:find [name description]
-                  :where [[_ :name name]
-                          [_ :description description]])))
+  [word]
+  (crux/q (crux/db database)
+          `{:find [id]
+            :where [[id :name ~word]]
+            :full-results? true}))
 
-(search-nodes "test")
-(add-node "Test" "This is a test.")
+(defn list-all-nodes
+  "List all nodes in the database."
+  []
+  (crux/q (crux/db database)
+          `{:find [id]
+            :where [id :crux.db/id _]}))
 
-(defn del-note
-  "Delete a node and the associated"
-  [])
+(defn node-by-id
+  "Fetch a node by its UUID."
+  [node-id]
+  (-> (crux/q (crux/db database)
+              `{:find [id]
+                :where [[id :crux.db/id ~node-id]]
+                :full-results? true})
+      first
+      first))
 
-(defn mod-note [])
+(defn node->text
+  "Converts a node to its textual representation."
+  [node-datastructure]
+  (->> node-datastructure
+       first
+       (map (fn [kv-pair]
+              (let [[key
+                     val] kv-pair]
+                [key (if (set? val)
+                       (->> val
+                            (map #(crux/q (crux/db database)
+                                          `{:find [object name]
+                                            :where [[id :crux.db/id ~%1]
+                                                    [id :name name]]}))
+                            (map first)
+                            (into #{}))
+                       val)])))
+       (map (fn [kv-pair]
+              (let [[key
+                     val] kv-pair]
+                (if (set? val)
+                  (->> val
+                       (map (fn [edge] (str
+                                        (subs (str key) 1)
+                                        ": " (second edge) " id: " (first edge))))
+                       (str/join "\n"))
+                  (str (subs (str key) 1) ": " val)))))
+       (str/join "\n")))
 
-(defn -main []
-  (println "hello world"))
+(defn link-nodes
+  "Link two nodes given their UUIDs."
+  [src dest type]
+  (crux/submit-tx database
+                  [[:crux.tx/put
+                    (assoc (node-by-id src)
+                           type [dest])]]))
+
+(defn list-nodes
+  "List all the nodes with a name."
+  [name]
+  (->> name
+       search-nodes
+       (map node->text)
+       (str/join "\n------\n")
+       (println)))
--- src/database/core.clj
+++ src/database/core.clj
@@ -1,6 +1,11 @@
 (ns database.core
   "The core set of functionality associated with the database."
-  (:require [crux.api :as crux]))
+  (:require [crux.api :as crux])
+  (:import [java.util UUID]))
+
+(def truth-uuid (UUID/randomUUID))
+(def science-uuid  (UUID/randomUUID))
+(def materialism-uuid (UUID/randomUUID))
 
 (def -default-nodes
   [{:crux.db/id :name
@@ -14,14 +19,38 @@
    {:crux.db/id :type
     :name "Type"
     :description "Types are the data surrounding an edge."
-    :type :type}])
+    :type :type}
+   {:crux.db/id :synonym
+    :name "Synonym"
+    :description "Synonyms of a entry."
+    :type :type}
+   {:crux.db/id science-uuid
+    :name "Science"
+    :description "The only method for obtaining knowledge about the natural world."
+    :synonym #{truth-uuid
+               materialism-uuid}
+    :type :object}
+   {:crux.db/id truth-uuid
+    :name "Truth"
+    :description "A collection of data vis a vis some fact."
+    :synonym #{science-uuid
+               materialism-uuid}
+    :type :object}
+   {:crux.db/id materialism-uuid
+    :name "Philosophical Materialism"
+    :description "The belief that that all things in the world are material."
+    :synonym #{science-uuid
+               truth-uuid}
+    :type :object}])
 
 (defn init-new-db
   "Creates a new database instance."
   []
-  (crux/submit-tx (crux/start-node {})
-                  (map (fn [node] [:crux.tx/put node]) -default-nodes)))
+  (let [database (crux/start-node {})]
+    (crux/submit-tx database
+                    (map (fn [node] [:crux.tx/put node]) -default-nodes))
+    database))
 
-(defn )
+                                        ;(defn )
 
-(init-new-db)
+                                        ;(init-new-db)
